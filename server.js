042c4260e6fb418748e075241bf119a21cfd9fe9
VincentC <vincent65961@gmail.com>
2020-09-14 22:37:25 +1000

    v4

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ dotenv.config({ path: './config/config.env' });
 // initiliase express app
 const app = express();
 
-// serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 // allow us to use the bodyParser middleware
 app.use(express.json());
 
@@ -28,18 +26,11 @@ if (process.env.NODE_ENV === 'development') {
 // mount the router
 app.use('/api/v1/mashup', require('./routes/api'));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
-// see if we need to keep this...having a clash with the curret. Ask Sam
 // <=====================>
 // <===PRODUCTION ONLY===>
 // <=====================>
 if (process.env.NODE_ENV === 'production') {
-  // when in product the client/build folder will be our static folder
+  // when in production the client/build folder will be our static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
